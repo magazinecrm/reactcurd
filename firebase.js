e0588a50c405aa1c346b3c253c221e5de031eaa2
pjohny <34673267+magazinecrm@users.noreply.github.com>
2020-08-19 14:59:18 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import * as firebase from 'firebase'
 
 
 var firebaseConfig = {
-  apiKey: "AIzaSyADF4ohCdk160ywuVprEw8guPpXXogZzio",
-  authDomain: "react-crud-1cbb3.firebaseapp.com",
+  apiKey: "AIzaSyADFrEw8guPpXXogZzio",
+  authDomain: "react-cseapp.com",
   databaseURL: "https://react-crud-1cbb3.firebaseio.com",
   projectId: "react-crud-1cbb3",
-  storageBucket: "react-crud-1cbb3.appspot.com",
-  messagingSenderId: "930049169773",
-  appId: "1:930049169773:web:d9cb7ac8e4683211282264",
+  storageBucket: "react-crappspot.com",
+  messagingSenderId: "930049773",
+  appId: "1:930049169773:w7ac8e4683211282264",
   measurementId: "G-LHPNW5ZYM9"
 };
 var fireDb = firebase.initializeApp(firebaseConfig);
